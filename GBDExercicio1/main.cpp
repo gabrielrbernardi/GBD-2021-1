2380c87e31ced09687cef40fa8a52fb6994d8939
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2021-03-14 16:46:15 -0300

    Exercicio 1 finalizado

--- GBDExercicio1/main.cpp
+++ GBDExercicio1/main.cpp
@@ -28,55 +28,84 @@ int main()
         inputStringFile = "";
     }
     arquivoSaida.close();
-    cout << "Arquivo fechado com sucesso" << endl;
+    arquivoEntrada.close();
+    cout << "Arquivos fechado com sucesso" << endl;
 
     ifstream arquivoEntrada1("cep2.txt");
     int opcao, qtdResultados, qtdLinhas;
-    string termoBusca;
+    string termoBusca, resultado;
     while(1){
-        cout << "1 - Busca por Rua\n2 - Busca por Cidade\n3 - Busca por UF\n4 - Busca por CEP" << endl;
+        cout << "1 - Busca por Rua\n2 - Busca por Cidade\n3 - Busca por UF\n4 - Busca por CEP\n5 - Sair" << endl;
         cout << "Digite uma opcao: ";
         scanf("%d", &opcao);
         if(opcao == 5){
+            arquivoEntrada1.close();
             break;
         }
         cout << "Qual termo deseja buscar: ";
-        limpa();
+        limpa();                                                    //Limpa o buffer do teclado para a leitura da string de busca
         getline (cin, termoBusca);
         cout << termoBusca << endl;
-        limpa();
+        limpa();                                                    //Limpa o buffer do teclado para a leitura da string de busca
 
-        if(opcao == 1){
+        if(opcao == 1){                                             //Busca por rua
             qtdResultados = 0;                                      //Variavel para mostrar a quantidade de resultados encontrados
             arquivoEntrada1.clear();                                //Limpa o estado do ifstream antes de chamar a proxima instrucao para leitura do arquivo
             arquivoEntrada1.seekg(0);                               //Posiciona o cursor na primeira linha e primeiro caractere do arquivo
             qtdLinhas = 0;                                          //Variavel para mostrar a quatidade de linhas lidas no arquivoEntrada1
-            while(getline (arquivoEntrada1, strTemp)){
+            while(getline (arquivoEntrada1, resultado)){
                 qtdLinhas++;
-                strTemp = strTemp.substr(0, strTemp.find("|"));
+                strTemp = resultado.substr(0, resultado.find("|"));
                 if(strTemp.find(termoBusca, 0) != string::npos){
-                    cout << strTemp << endl;
+                    cout << resultado << endl;
                     qtdResultados++;
                 }
             }
-            cout << "Foram lidas " << qtdLinhas << " linhas" << endl;
-            cout << "Foram encontrados " << qtdResultados << " resultados" << endl;
-        }else if(opcao == 2){
+        }else if(opcao == 2){                                       //Busca por cidade
             qtdResultados = 0;                                      //Variavel para mostrar a quantidade de resultados encontrados
             arquivoEntrada1.clear();                                //Limpa o estado do ifstream antes de chamar a proxima instrucao para leitura do arquivo
             arquivoEntrada1.seekg(0);                               //Posiciona o cursor na primeira linha e primeiro caractere do arquivo
             qtdLinhas = 0;                                          //Variavel para mostrar a quatidade de linhas lidas no arquivoEntrada1
-            while(getline (arquivoEntrada1, strTemp)){
+            while(getline (arquivoEntrada1, resultado)){
                 qtdLinhas++;
-                strTemp = strTemp.substr(strTemp.find("|")+1, strTemp.end());
+                strTemp = resultado.substr(resultado.find("|")+1, resultado.size());
                 if(strTemp.find(termoBusca, 0) != string::npos){
-                    cout << strTemp << endl;
+                    cout << resultado << endl;
                     qtdResultados++;
                 }
             }
-            cout << "Foram lidas " << qtdLinhas << " linhas" << endl;
-            cout << "Foram encontrados " << qtdResultados << " resultados" << endl;
+        }else if(opcao == 3){                                       //Busca por UF
+            qtdResultados = 0;                                      //Variavel para mostrar a quantidade de resultados encontrados
+            arquivoEntrada1.clear();                                //Limpa o estado do ifstream antes de chamar a proxima instrucao para leitura do arquivo
+            arquivoEntrada1.seekg(0);                               //Posiciona o cursor na primeira linha e primeiro caractere do arquivo
+            qtdLinhas = 0;                                          //Variavel para mostrar a quatidade de linhas lidas no arquivoEntrada1
+            while(getline (arquivoEntrada1, resultado)){
+                qtdLinhas++;
+                strTemp = resultado.substr(resultado.find("|")+1, resultado.size());
+                strTemp = strTemp.substr(strTemp.find("|")+1, strTemp.size());
+                if(strTemp.find(termoBusca, 0) != string::npos){
+                    cout << resultado << endl;
+                    qtdResultados++;
+                }
+            }
+        }else if(opcao == 4){                                       //Busca por CEP
+            qtdResultados = 0;                                      //Variavel para mostrar a quantidade de resultados encontrados
+            arquivoEntrada1.clear();                                //Limpa o estado do ifstream antes de chamar a proxima instrucao para leitura do arquivo
+            arquivoEntrada1.seekg(0);                               //Posiciona o cursor na primeira linha e primeiro caractere do arquivo
+            qtdLinhas = 0;                                          //Variavel para mostrar a quatidade de linhas lidas no arquivoEntrada1
+            while(getline (arquivoEntrada1, resultado)){
+                qtdLinhas++;
+                strTemp = resultado.substr(resultado.find("|")+1, resultado.size());
+                strTemp = strTemp.substr(strTemp.find("|")+1, strTemp.size());
+                strTemp = strTemp.substr(strTemp.find("|")+1, strTemp.size());
+                if(strTemp.find(termoBusca, 0) != string::npos){
+                    cout << resultado << endl;
+                    qtdResultados++;
+                }
+            }
         }
 
+        cout << "Foram lidas " << qtdLinhas << " linhas" << endl;
+        cout << "Foram encontrados " << qtdResultados << " resultados" << endl;
     }
 }
