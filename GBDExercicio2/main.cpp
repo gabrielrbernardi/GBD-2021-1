acbff74e60af8eb60f6331c46ce1253ff93ca416
Guilherme Soares Correa <55603259+guisoares1@users.noreply.github.com>
2021-03-21 20:42:00 -0300

    Fim

--- GBDExercicio2/main.cpp
+++ GBDExercicio2/main.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
+#include <ctype.h>
+/********
+Integrantes do grupo
+Bruno Oliveira Sinhoroto - 11821BCC044
+Gabriel Ribeiro Bernardi - 11821BCC036
+Guilherme Soares Correa  - 11821BCC026
+*********/
 using namespace std;
-
 void limpa(){
     setbuf(stdin, NULL);
 }
@@ -16,71 +22,119 @@ struct item{
 char *padronizatamanhocampos(char *texto, int tamanho){
     int cont=0;
     for(cont=0;cont<tamanho-1;cont++){
-        if(texto[cont]=='\0'){
-           texto[cont]=' ';
-           texto[cont+1]='\0';
+        if(texto[cont]=='\0'){ // verifica se chegou no final do arquivo
+           texto[cont]=' ';    // adiciona espaço até padronizar o tamanho da struct.
+           texto[cont+1]='\0'; // coloca o final do arquivo manualmente para os dados não se perderem.
         }
     }
     return texto;
 }
 
-int separaStringDados(string dados, struct item *item){
+char *semespaco(char *texto, int tamanho){
+    int cont=0, cont2=0, posfin=0;
+    //percorro para verificar a ultima ocorrencia de caractere, ignorando espços
+    for(cont=0;cont<tamanho-1;cont++){
+        if ((texto[cont]!='\0')&&(!(texto[cont] == ' ' || texto[cont] == '\t' || texto[cont] == '\r' || texto[cont] == '\n' || texto[cont] == '\x0b')))
+            posfin = cont;
+    }
+    // criando nova variável sem os espaços.
+    texto[posfin+1] = '\0';
+    return texto;
+}
+
+// Compara manualmente o texto até o '\0' (final do char)
+int comaparaChar(char *texto1, const char *texto2){
+    int cont=0;
+    cout<< texto1;
+    while (texto1[cont]!='\0'){
+        cout << texto1[cont] << texto2[cont] << endl;
+        if (texto1[cont]!=texto2[cont])
+            return 0;
+        cont++;
+    }
+    return 1;
+}
+
+int separaStringDados(string dados, struct item *item, int posCep, int posUf, int posCid, int posLog){
     string cep, uf, cidade, logradouro;
-    cep = dados.substr(0, dados.find("\t"));
-    dados.erase(0, dados.find("\t")+1);
-    uf = dados.substr(0, dados.find("\t"));
+    try{
+        item->cep = stoi(dados.substr(0, ((posCep == 0) ? dados.find("\t") : posCep)));
+    }catch(...){
+        return 1;
+    }
+    dados.erase(0, ((posCep == 0) ? dados.find("\t")+1 : posCep));
+    uf = dados.substr(0, ((posUf == 0) ? dados.find("\t") : posUf));
     strcpy(item->uf, uf.c_str());
-    dados.erase(0, dados.find("\t")+1);
-    cidade = dados.substr(0, dados.find("\t"));
+    dados.erase(0, ((posUf == 0) ? dados.find("\t")+1 : posUf));
+    cidade = dados.substr(0, ((posCid == 0) ? dados.find("\t") : posCid));
     strcpy(item->cidade, cidade.c_str());
-    dados.erase(0, dados.find("\t")+1);
-    logradouro = dados.substr(0, dados.find("\t"));
+    dados.erase(0, ((posCid == 0) ? dados.find("\t")+1 : posCid));
+    logradouro = dados.substr(0, ((posLog == 0) ? dados.find("\t") : posCid));
     strcpy(item->logradouro, logradouro.c_str());
-    dados.erase(0, dados.find("\t")+1);
-    return((item->cidade[0] == '\0') ? 1 : 0);
+    dados.erase(0, ((posLog == 0) ? dados.find("\t")+1 : posLog));
+    return(0);
 }
 
-void finaliza(FILE *arquivoEntrada,FILE *arquivoSaida){
-    fclose(arquivoEntrada);
-    fclose(arquivoSaida);
+void pesquisa(FILE *arquivoSaida, int opPes, string descPes)
+{   int numEnc=0;
+    char cepChar[8], leitura;
+    struct item percorre;
+    string dadosP;
+    while(1){
+        while(fread(&leitura, sizeof(char), 1, arquivoSaida) == 1 && leitura != '\n'){
+            dadosP.push_back(leitura);
+        }
+        if (separaStringDados(dadosP, &percorre, 8, 3, 39, 67)){
+           break;}
+        sprintf(cepChar,"%d",percorre.cep); // transforma int em char;
+        if (opPes==1){
+            if (semespaco(cepChar, sizeof(cepChar)) == descPes)
+                {numEnc++;}}
+        else if (opPes==2){
+            if (semespaco(percorre.cidade,sizeof(percorre.cidade)) == descPes)
+                {numEnc++;}
+        }
+        else if (opPes==3){
+            if (semespaco(percorre.logradouro,sizeof(percorre.logradouro)) == descPes)
+                {numEnc++;}
+        }
+        else if (opPes==4){
+            if (semespaco(percorre.uf,sizeof(percorre.uf)) == descPes)
+                {numEnc++;}
+        }
+        dadosP.erase(0, dadosP.size());
+    }
+    cout<< "Registros encontrados na pesquisa: " << numEnc << endl;
 }
-
-//void pesquisa()
-//{
-//    while(1){
-//        while(fread(&c, sizeof(char), 1, arquivoEntrada) == 1 && c != '\n'){
-//            dados.push_back(c);
-//        }
-//        if (separaStringDados(dados, &item)){
-//           finaliza(arquivoEntrada,arquivoSaida);
-//           return 0;}
-//           }
-//}
 
 int main()
 {
-    FILE *arquivoEntrada = fopen("cep3.txt", "r");
-    FILE *arquivoSaida = fopen("cep2.txt", "w");
-
     string inputStringFile;
     string breakString = "\t";
     string substring;
     struct item item;
     long long lSize;
-    int qtdLinhas = 0;
+    int qtdLinhas = 0, opcao, cont=0;
     char cepescrita[8];
+    string termoBusca;
 
     char c;
     string dados;
     // escrita no novo arquivo
-    if (1){
+    cout << "Trabalho, 1 - topico 1, 2 - topico 2, 3 - topico 3 "<<endl;
+    scanf("%d", &opcao);
+
+    if (opcao==1){
+        FILE *arquivoEntrada = fopen("cep3.txt", "r");
+        FILE *arquivoSaida = fopen("cep2.bin", "wb");
         while(1){
             while(fread(&c, sizeof(char), 1, arquivoEntrada) == 1 && c != '\n'){
                 dados.push_back(c);
             };
-            if (separaStringDados(dados, &item)){
-               cout << "Foram lidas " << qtdLinhas << " linhas" << endl;
-               finaliza(arquivoEntrada,arquivoSaida);
+            if (separaStringDados(dados, &item, 0, 0, 0, 0)){
+                cout << "Foram lidas " << qtdLinhas << " linhas" << endl;
+                fclose(arquivoEntrada);
+                fclose(arquivoSaida);
                return 0;}
             qtdLinhas++;
             sprintf(cepescrita,"%d",item.cep); // transforma int em char;
@@ -90,145 +144,45 @@ int main()
             fwrite(padronizatamanhocampos(item.logradouro,sizeof(item.logradouro)) , sizeof(char) , sizeof(item.logradouro) , arquivoSaida );
             fwrite("\n", sizeof(char) , 1 , arquivoSaida );
             dados.erase(0, dados.size());
-        }}
-    else{
+        }
+        fclose(arquivoEntrada);
+        fclose(arquivoSaida);}
+    else if (opcao==2){
         // busca sequencial no arquivo criado
-//        while(1){
-//            cout << "1 - Busca por Rua\n2 - Busca por Cidade\n3 - Busca por UF\n4 - Busca por CEP\n5 - Sair" << endl;
-//            cout << "Digite uma opcao: ";
-//            scanf("%d", &opcao);
-//            if(opcao == 5){                                             //Opcao para encerra aplicacao
-//                break;
-//            }
-//            cout << "Qual termo deseja buscar: ";
-//            limpa();                                                    //Limpa o buffer do teclado para a leitura da string de busca
-//            getline (cin, termoBusca);
-//            cout << termoBusca << endl;
-//            limpa();
-//            }}
-}
-//    while(!feof(arquivoEntrada)){
-//        fread(pedro, 1, lSize, arquivoEntrada);
-//        cout << pedro << endl;
-//    }
+        FILE *arquivoSaida = fopen("cep2.bin", "r");
+        while(1){
+            cout << "1 - Busca por CEP\n2 - Busca por Cidade\n3 - Busca por Rua\n4 - Busca por UF\n5 - Sair" << endl;
+            cout << "Digite uma opcao: ";
+            scanf("%d", &opcao);
+            if(opcao == 5){                                             //Opcao para encerra aplicacao
+                break;
+            }
+            cout << "Qual termo deseja buscar: ";
+            limpa();                                                    //Limpa o buffer do teclado para a leitura da string de busca
+            getline (cin, termoBusca);
+            cout << termoBusca << endl;
+            limpa();
 
-//    while(!feof(arquivoEntrada)){
-//        fread(pedro, 1, lSize, arquivoEntrada);
-//        int posicaoTabulacao;
-//        string strTemp(pedro);
-//        posicaoTabulacao = strTemp.find(breakString);           //Recebe a posicao da tabulacao
-//        substring = strTemp.substr(0, posicaoTabulacao);        //Pega o cep
-//        item.cep = stoi(substring);
-//
-//        strTemp.erase(0, posicaoTabulacao+1);                   //Exclui campo + '\t'
-//        posicaoTabulacao = strTemp.find(breakString);           //Recebe a posicao da tabulacao
-//        substring = strTemp.substr(0, posicaoTabulacao);        //Pega a UF
-//        strcpy(item.uf, substring.c_str());
-//
-//        strTemp.erase(0, posicaoTabulacao+1);                   //Exclui campo + '\t'
-//        posicaoTabulacao = strTemp.find(breakString);           //Recebe a posicao da tabulacao
-//        substring = strTemp.substr(0, posicaoTabulacao);        //Pega a UF
-//        strcpy(item.cidade, substring.c_str());
-//
-//        strTemp.erase(0, posicaoTabulacao+1);                   //Exclui campo + '\t'
-//        posicaoTabulacao = strTemp.find(breakString);           //Recebe a posicao da tabulacao
-//        substring = strTemp.substr(0, posicaoTabulacao);        //Pega a UF
-//
-//        cout << item.cep << endl;
-//        cout << item.uf << endl;
-//        cout << item.cidade << endl;
-//        cout << item.logradouro << endl;
-//
-//        fwrite(item.uf, 1, sizeof(item.uf), arquivoEntrada);
-//
-////        strTemp.erase(0, posicaoTabulacao+1);                   //Exclui campo + '\t'
-////        inputStringFile.insert(0, substring);
-////        cout << inputStringFile << endl;
-////        arquivoSaida << inputStringFile << endl;                    //Escrita da string no arquivo de saida
-////        inputStringFile = "";
-//    }
-    //Fecha arquivos de entrada e sa�da
-//    arquivoSaida.close();
-//    cout << "Arquivo 1 encerrado com sucesso" << endl;
-//    arquivoEntrada.close();
-//    cout << "Arquivo 2 encerrado com sucesso" << endl;
-
-//    ifstream arquivoEntrada1("cep2.txt");                           //Abre arquivo para leitura
-//    int opcao, qtdResultados, qtdLinhas;
-//    string termoBusca, resultado;
-//    while(1){
-//        cout << "1 - Busca por Rua\n2 - Busca por Cidade\n3 - Busca por UF\n4 - Busca por CEP\n5 - Sair" << endl;
-//        cout << "Digite uma opcao: ";
-//        scanf("%d", &opcao);
-//        if(opcao == 5){                                             //Opcao para encerra aplicacao
-//            arquivoEntrada1.close();                                //Encerra arquivo de leitura
-//            cout << "Programa finalizado" << endl;
-//            break;
-//        }
-//        cout << "Qual termo deseja buscar: ";
-//        limpa();                                                    //Limpa o buffer do teclado para a leitura da string de busca
-//        getline (cin, termoBusca);
-//        cout << termoBusca << endl;
-//        limpa();                                                    //Limpa o buffer do teclado para a leitura da string de busca
-//
-//        if(opcao == 1){                                             //Busca por rua
-//            qtdResultados = 0;                                      //Variavel para mostrar a quantidade de resultados encontrados
-//            arquivoEntrada1.clear();                                //Limpa o estado do ifstream antes de chamar a proxima instrucao para leitura do arquivo
-//            arquivoEntrada1.seekg(0);                               //Posiciona o cursor na primeira linha e primeiro caractere do arquivo
-//            qtdLinhas = 0;                                          //Variavel para mostrar a quatidade de linhas lidas no arquivoEntrada1
-//            while(getline (arquivoEntrada1, resultado)){
-//                qtdLinhas++;
-//                strTemp = resultado.substr(0, resultado.find("|"));
-//                if(strTemp.find(termoBusca, 0) != string::npos){
-//                    cout << resultado << endl;
-//                    qtdResultados++;
-//                }
-//            }
-//        }else if(opcao == 2){                                       //Busca por cidade
-//            qtdResultados = 0;                                      //Variavel para mostrar a quantidade de resultados encontrados
-//            arquivoEntrada1.clear();                                //Limpa o estado do ifstream antes de chamar a proxima instrucao para leitura do arquivo
-//            arquivoEntrada1.seekg(0);                               //Posiciona o cursor na primeira linha e primeiro caractere do arquivo
-//            qtdLinhas = 0;                                          //Variavel para mostrar a quatidade de linhas lidas no arquivoEntrada1
-//            while(getline (arquivoEntrada1, resultado)){
-//                qtdLinhas++;
-//                strTemp = resultado.substr(resultado.find("|")+1, resultado.size());
-//                if(strTemp.find(termoBusca, 0) != string::npos){
-//                    cout << resultado << endl;
-//                    qtdResultados++;
-//                }
-//            }
-//        }else if(opcao == 3){                                       //Busca por UF
-//            qtdResultados = 0;                                      //Variavel para mostrar a quantidade de resultados encontrados
-//            arquivoEntrada1.clear();                                //Limpa o estado do ifstream antes de chamar a proxima instrucao para leitura do arquivo
-//            arquivoEntrada1.seekg(0);                               //Posiciona o cursor na primeira linha e primeiro caractere do arquivo
-//            qtdLinhas = 0;                                          //Variavel para mostrar a quatidade de linhas lidas no arquivoEntrada1
-//            while(getline (arquivoEntrada1, resultado)){
-//                qtdLinhas++;
-//                strTemp = resultado.substr(resultado.find("|")+1, resultado.size());
-//                strTemp = strTemp.substr(strTemp.find("|")+1, strTemp.size());
-//                if(strTemp.find(termoBusca, 0) != string::npos){
-//                    cout << resultado << endl;
-//                    qtdResultados++;
-//                }
-//            }
-//        }else if(opcao == 4){                                       //Busca por CEP
-//            qtdResultados = 0;                                      //Variavel para mostrar a quantidade de resultados encontrados
-//            arquivoEntrada1.clear();                                //Limpa o estado do ifstream antes de chamar a proxima instrucao para leitura do arquivo
-//            arquivoEntrada1.seekg(0);                               //Posiciona o cursor na primeira linha e primeiro caractere do arquivo
-//            qtdLinhas = 0;                                          //Variavel para mostrar a quatidade de linhas lidas no arquivoEntrada1
-//            while(getline (arquivoEntrada1, resultado)){
-//                qtdLinhas++;
-//                strTemp = resultado.substr(resultado.find("|")+1, resultado.size());
-//                strTemp = strTemp.substr(strTemp.find("|")+1, strTemp.size());
-//                strTemp = strTemp.substr(strTemp.find("|")+1, strTemp.size());
-//                if(strTemp.find(termoBusca, 0) != string::npos){
-//                    cout << resultado << endl;
-//                    qtdResultados++;
-//                }
-//            }
-//        }
-//
-//        cout << "Foram lidas " << qtdLinhas << " linhas" << endl;
-//        cout << "Foram encontrados " << qtdResultados << " resultados" << endl;
-//    }
+            pesquisa(arquivoSaida, opcao, termoBusca);
+            rewind(arquivoSaida);
+            }
+        fclose(arquivoSaida);
+        }else if (opcao==3){
+            FILE *arquivoSaida = fopen("cep2.bin", "r");
+            // busca por linha
+            cout << "Digite a linha a ser buscada: ";
+            scanf("%d", &opcao);
+            while(fread(&c, sizeof(char), 1, arquivoSaida) == 1 && c != '\n'){
+                dados.push_back(c);
+            };
+            cout<<sizeof(dados)<<endl;
+            if (separaStringDados(dados, &item, 8, 3, 39, 67)){
+                cout << "Foram lidas " << qtdLinhas << " linhas" << endl;
+                return 0;}
+            cout<<"Cep:    "<<item.cep <<endl;
+            cout<<"Cidade: "<<item.cidade<<endl;
+            cout<<"UF:     "<<item.uf <<endl;
+            cout<<"Rua:    "<<item.logradouro<<endl;
+            fclose(arquivoSaida);
+          }
 }
